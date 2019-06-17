d33d3d531c7902042bc743a59ce178e05759b0ba
nilesh.korde <nilesh.korde@MUS-DVW-DB01>
2019-06-17 07:25:33 +0100

    Bug fix for file upload issue

--- src/app/hostedapp/hostedapp.component.ts
+++ src/app/hostedapp/hostedapp.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { FileUploadService } from '../file-upload-service.service';
-import { HttpClient,HttpHeaders} from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Router } from '@angular/router';
-import {  DashboardService } from '../services/dashboardCall.service';
+import { DashboardService } from '../services/dashboardCall.service';
 
 @Component({
   selector: 'app-hostedapp',
@@ -11,116 +11,84 @@ import {  DashboardService } from '../services/dashboardCall.service';
   styleUrls: ['./hostedapp.component.css']
 })
 export class HostedappComponent implements OnInit {
-
   profileForm: FormGroup;
+  categoryArr = [];
+  Icon = null;
+  AndriodSmartPhoneBuild = null;
+  AndriodTabletBuild = null;
+  IphoneBuild = null;
+  IpadBuild = null;
+  ScreenShots = null;
+  Documents = null;
+  categoryId = null;
+  showMessage = false;
+  hostedAppTab = true;
+  thirtPartyAppTab = false;
+  webAppTab = false;
+  documentAppTab = false;
+  User = '';
+  firstName = '';
+  lastname = '';
 
-  categoryArr=[];
-
-  Icon=null;
-  AndriodSmartPhoneBuild=null;
-  AndriodTabletBuild=null;
-  IphoneBuild=null;
-  IpadBuild=null;
-  ScreenShots=null;
-  Documents=null;
-  categoryId=null;
-  showMessage=false;
-
-  hostedAppTab=true;  
-  thirtPartyAppTab=false;
-  webAppTab=false;
-  documentAppTab=false;
-  User='';
-  firstName='';
-  lastname='';
-
-  constructor(private dashboardService : DashboardService,private fb: FormBuilder, private fileUploadService: FileUploadService,private http: HttpClient,private router :Router) { }
+  constructor(private dashboardService: DashboardService, private fb: FormBuilder, private fileUploadService: FileUploadService, private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
-    // this.profileForm = this.fb.group({
-    //   name: [''],
-    //   profile: ['']
-    // });
     this.getAppCategory();
-    this.User=JSON.parse(localStorage.getItem('currentUser'));
-    // console.log(this.User);
-    this.firstName=this.User['Firstname'];
-    this.lastname=this.User['Lastname'];
-
-
-    this.dashboardService.setUserdetails(this.firstName+ ' ' + this.lastname);
+    this.User = JSON.parse(localStorage.getItem('currentUser'));
+    this.firstName = this.User['Firstname'];
+    this.lastname = this.User['Lastname'];
+    this.dashboardService.setUserdetails(this.firstName + ' ' + this.lastname);
   }
 
-  
-  
+  getAppCategory() {
 
-  getAppCategory(){   
-  
-      this.fileUploadService.getCategory().subscribe(res=>
-        // console.log(res));
-      this.categoryArr=res
-      )
-  
-    }
+    this.fileUploadService.getCategory().subscribe(res =>
+      this.categoryArr = res
+    )
+  }
 
-  getCategoryId(category){
-    this.categoryId=+category.value;
+  getCategoryId(category) {
+    this.categoryId = +category.value;
     console.log(this.categoryId);
-  }
-
-  // Event calls for - files
+  }  
 
   onIcon(event) {
     console.log(event);
-    this.Icon = event.target.files[0];     
+    this.Icon = event.target.files[0];
   }
 
   onAndriodSmartPhoneBuild(event) {
     console.log(event);
-    this.AndriodSmartPhoneBuild = event.target.files[0];     
+    this.AndriodSmartPhoneBuild = event.target.files[0];
   }
 
   onAndriodTabletBuild(event) {
     console.log(event);
-    this.AndriodTabletBuild = event.target.files[0];     
+    this.AndriodTabletBuild = event.target.files[0];
   }
 
   onIphoneBuild(event) {
     console.log(event);
-    this.IphoneBuild = event.target.files[0];     
+    this.IphoneBuild = event.target.files[0];
   }
 
   onIpadBuild(event) {
     console.log(event);
-    this.IpadBuild = event.target.files[0];     
+    this.IpadBuild = event.target.files[0];
   }
 
   onScreenShots(event) {
     console.log(event);
-    this.ScreenShots = event.target.files;     
+    this.ScreenShots = event.target.files;
   }
 
   onDocuments(event) {
     console.log(event);
-    this.Documents = event.target.files;     
+    this.Documents = event.target.files;
   }
-  //End of  Event calling in different files
-  
 
-
-  //onSbmit function starts
-
-
-  onSubmit(CategoryId,Title,Description,Version,IphonePackageName,IpadPackageName) {
-    const formData = new FormData();  
-
-    // console.log(CategoryId.value);
-    // console.log(Title.value);
-    // console.log(Description.value);
-    // console.log(Version.value);
-    // console.log(IphonePackageName.value);
-    // console.log(IpadPackageName.value);
-   
+  onSubmit(CategoryId, Title, Description, Version, IphonePackageName, IpadPackageName) {
+    const formData = new FormData();
     if (this.Documents != null) {
       for (let i = 0; i < this.Documents.length; i++) {
         formData.append('Documents', this.Documents[i], this.Documents[i].name);
@@ -132,66 +100,38 @@ export class HostedappComponent implements OnInit {
         formData.append('ScreenShots', this.ScreenShots[i], this.ScreenShots[i].name);
       }
     }
-    
-  
-    formData.append('Icon', this.Icon,this.Icon.name);
-    formData.append('AndriodSmartPhoneBuild', this.AndriodSmartPhoneBuild,this.AndriodSmartPhoneBuild.name);
-    formData.append('AndriodTabletBuild', this.AndriodTabletBuild,this.AndriodTabletBuild.name);
-    formData.append('IphoneBuild', this.IphoneBuild,this.IphoneBuild.name);
-    formData.append('IpadBuild', this.IpadBuild,this.IpadBuild.name);
-    // formData.append('ScreenShots', this.ScreenShots,this.ScreenShots.name);
-    // formData.append('Documents', this.Documents,this.Documents.name);
-
-
+    if (this.Icon != null) {
+      formData.append('Icon', this.Icon, this.Icon.name);
+    }
+    if (this.AndriodSmartPhoneBuild != null) {
+      formData.append('AndriodSmartPhoneBuild', this.AndriodSmartPhoneBuild, this.AndriodSmartPhoneBuild.name);
+    }
+    if (this.AndriodTabletBuild != null) {
+      formData.append('AndriodTabletBuild', this.AndriodTabletBuild, this.AndriodTabletBuild.name);
+    }
+    if (this.IphoneBuild != null) {
+      formData.append('IphoneBuild', this.IphoneBuild, this.IphoneBuild.name);
+    }
+    if (this.IpadBuild != null) {
+      formData.append('IpadBuild', this.IpadBuild, this.IpadBuild.name);
+    }
     formData.append('CategoryId', CategoryId.value);
     formData.append('Title', Title.value);
     formData.append('Description', Description.value);
     formData.append('Version', Version.value);
     formData.append('IphonePackageName', IphonePackageName.value);
     formData.append('IpadPackageName', IpadPackageName.value);
-
-    
     formData.append('published', 'true');
 
-
-    
-    
-
     console.log(formData);
-    // formData=this.userdata;
 
-    /////
-    this.fileUploadService.upload(formData).subscribe(res=>{
-      console.log("data update: "+ res);
-      this.showMessage=true;
-
-      // title=null;
-      // description=null;
-      // version=null;
-      // iphonepackage=null;
-      // ipadpackage=null;
-      // this.AppIconUploadFile=null;
-      // this.androidbuildUploadFile=null;
+    this.fileUploadService.upload(formData).subscribe(res => {
+      console.log("data update: " + res);
+      this.showMessage = true;
     }
-    
     );
-    ///////
-
-
-    
   }
-
-  //onSbmit function ends
-
-
-  //backToHome function starts
-
-  backToHome(){
-      this.router.navigate(['/'])
+  backToHome() {
+    this.router.navigate(['/'])
   }
-
-    //backToHome function ends
-
-
-
 }
