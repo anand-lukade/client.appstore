f0a2fe7dd95762e7e95743d5e9636692f715aaf3
nilesh.korde <nilesh.korde@MUS-DVW-DB01>
2019-06-17 06:05:48 +0100

    Amit did changed theme

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -42,7 +42,7 @@ export class AuthenticationService {
     logout() {
         // remove user from local storage to log user out
         localStorage.removeItem('currentUser');
-        this.loggedIn.next(false);
+        // this.loggedIn.next(false);
         this.router.navigate(['/login']);
     }
 }
